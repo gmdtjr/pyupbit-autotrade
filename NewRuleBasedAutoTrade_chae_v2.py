4c3a13d32405905d1f64d423cfe804b1fe99f2b1
gmdtjr <79096878+gmdtjr@users.noreply.github.com>
2022-02-05 10:05:08 +0900

    Add files via upload

--- NewRuleBasedAutoTrade_chae_v2.py
+++ NewRuleBasedAutoTrade_chae_v2.py
@@ -24,28 +24,143 @@ def get_current_price(ticker):
     #"""Current Price Check"""
     return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
 
+def strategy(ticker, tic, kkk, state, buy_price, ratio):
+
+    current_price = get_current_price(ticker)
+
+    # Data Loading
+    df = pyupbit.get_ohlcv(ticker, interval="minute60", count = 30)
+    # Mean Price 
+    low_1 = float(df['low'][-2:-1])
+    low_2 = float(df['low'][-3:-2])
+    low_3 = float(df['low'][-4:-3])
+    low_mean = (low_1 + low_2 + low_3)/3
+
+    # High Low Price 
+    high_min = 999999999999999
+    low_min = 999999999999999
+    for i in range(-24,0):
+        high_temp = float(df['high'][-1+i:0+i])
+        if (high_min > high_temp):
+            high_min = high_temp     
+    for i in range(-24,0):
+        low_temp = float(df['low'][-1+i:0+i])
+        if (low_min > low_temp):
+            low_min = low_temp         
+     
+    # Buy Strategy - HUNT
+    if (state == 0):
+        if (current_price < low_mean and current_price > high_min):
+            krw = (get_balance("KRW"))
+            krw = ratio*krw
+            if krw > 5000:
+                upbit.buy_market_order(ticker, krw*0.9995)
+                state = 1
+            buy_price = current_price
+            kkk = 0
+
+    # Sell Strategy - HUNT
+    if (state == 1):
+        if (kkk <= 360): ## 1시간 경과
+            percent = 0.015 - 0.015*(kkk/360)
+            target_price = buy_price*(1+percent)
+            percent = 0.01 - 0.01*(kkk/360)
+            lower_price = buy_price*(1-percent)
+            if (current_price > target_price): 
+                if (not (current_price < low_mean and current_price > high_min)):
+                    btc = get_balance(tic)
+                    if btc > 0:
+                        upbit.sell_market_order(ticker, btc*0.9995)
+                        1
+                    state = 0
+                    kkk = 0
+                else:
+                    buy_price = current_price
+                    state = 1
+                    kkk = 0
+            elif (current_price < lower_price):
+                buy_price = current_price
+                state = 1
+        else:
+            buy_price = current_price
+            state = 1
+            kkk = 360
+        
+        if (current_price < low_min):
+            btc = get_balance(tic)
+            if btc > 0:
+                upbit.sell_market_order(ticker, btc*0.9995)
+                1
+            state = 0
+            kkk = 0
+
+    return kkk, state, buy_price
+
 # Log-In
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
 # Initial flag setting
-ticker_A = "KRW-BTC"
-ticker_B = "KRW-ETH"
-ticker_C = "KRW-STRK"
-tic_A = "BTC"
-tic_B = "ETH"
-tic_C = "STRK"
-state_A = 0
-state_B = 0 
-state_C = 0 
-buy_price_A = 0
-buy_price_B = 0
-buy_price_C = 0
-target_price = 0.0
-lower_price = 0.0
-kkk_A = 0
-kkk_B = 0
-kkk_C = 0
+ticker_1 = "KRW-BTC"
+ticker_2 = "KRW-ETH"
+ticker_3 = "KRW-STRK"
+ticker_4 = "KRW-HUNT"
+ticker_5 = "KRW-JST"
+ticker_6 = "KRW-BORA"
+ticker_7 = "KRW-XRP"
+ticker_8 = "KRW-SAND"
+ticker_9 = "KRW-SOL"
+ticker_10 = "KRW-ONT"
+tic_1 = "BTC"
+tic_2 = "ETH"
+tic_3 = "STRK"
+tic_4 = "HUNT"
+tic_5 = "JST"
+tic_6 = "BORA"
+tic_7 = "XRP"
+tic_8 = "SAND"
+tic_9 = "SOL"
+tic_10 = "ONT"
+ratio_1 = 0.9
+ratio_2 = 0.6
+ratio_3 = 0.3
+ratio_4 = 0.2
+ratio_5 = 0.4
+ratio_6 = 0.2
+ratio_7 = 0.2
+ratio_8 = 0.3
+ratio_9 = 0.3
+ratio_10 = 0.2
+state_1 = 0
+state_2 = 0 
+state_3 = 0 
+state_4 = 0
+state_5 = 0 
+state_6 = 0 
+state_7 = 0
+state_8 = 0 
+state_9 = 0 
+state_10 = 0
+buy_price_1 = 0
+buy_price_2 = 0
+buy_price_3 = 0
+buy_price_4 = 0
+buy_price_5 = 0
+buy_price_6 = 0
+buy_price_7 = 0
+buy_price_8 = 0
+buy_price_9 = 0
+buy_price_10 = 0
+kkk_1 = 0
+kkk_2 = 0
+kkk_3 = 0
+kkk_4 = 0
+kkk_5 = 0
+kkk_6 = 0
+kkk_7 = 0
+kkk_8 = 0
+kkk_9 = 0
+kkk_10 = 0
 
 now = datetime.datetime.now()
 print(now.hour, now.minute, 30%24)
@@ -56,190 +171,48 @@ while True:
     try:
         # Time Update
         now = datetime.datetime.now()
-        if (now.minute != minute_pre):
-            kkk_A = kkk_A + 1
-            kkk_B = kkk_B + 1
-            kkk_C = kkk_C + 1
         schedule.run_pending()
-        current_price_A = get_current_price(ticker_A)
-        current_price_B = get_current_price(ticker_B)
-        current_price_C = get_current_price(ticker_C)
 
-        # Data Loading
-        df_A = pyupbit.get_ohlcv("KRW-BTC", interval="minute60", count = 30)
-        df_B = pyupbit.get_ohlcv("KRW-ETH", interval="minute60", count = 30)
-        df_C = pyupbit.get_ohlcv("KRW-STRK", interval="minute60", count = 30)
-        # Mean Price 
-        low_1_A = float(df_A['low'][-2:-1])
-        low_2_A = float(df_A['low'][-3:-2])
-        low_3_A = float(df_A['low'][-4:-3])
-        low_mean_A = (low_1_A + low_2_A + low_3_A)/3
-
-        
-        low_1_B = float(df_B['low'][-2:-1])
-        low_2_B = float(df_B['low'][-3:-2])
-        low_3_B = float(df_B['low'][-4:-3])
-        low_mean_B = (low_1_B + low_2_B + low_3_B)/3
-
-        low_1_C = float(df_C['low'][-2:-1])
-        low_2_C = float(df_C['low'][-3:-2])
-        low_3_C = float(df_C['low'][-4:-3])
-        low_mean_C = (low_1_C + low_2_C + low_3_C)/3
-
-        # High Low Price 
-        high_min_A = 999999999999999
-        low_min_A = 999999999999999
-        for i in range(-24,0):
-            high_temp = float(df_A['high'][-1+i:0+i])
-            if (high_min_A > high_temp):
-                high_min_A = high_temp     
-        for i in range(-24,0):
-            low_temp = float(df_A['low'][-1+i:0+i])
-            if (low_min_A > low_temp):
-                low_min_A = low_temp         
-
-        high_min_B = 999999999999999
-        low_min_B = 999999999999999
-        for i in range(-24,0):
-            high_temp = float(df_B['high'][-1+i:0+i])
-            if (high_min_B > high_temp):
-                high_min_B = high_temp     
-        for i in range(-24,0):
-            low_temp = float(df_B['low'][-1+i:0+i])
-            if (low_min_B > low_temp):
-                low_min_B = low_temp      
-
-        high_min_C = 999999999999999
-        low_min_C = 999999999999999
-        for i in range(-24,0):
-            high_temp = float(df_C['high'][-1+i:0+i])
-            if (high_min_C > high_temp):
-                high_min_C = high_temp     
-        for i in range(-24,0):
-            low_temp = float(df_C['low'][-1+i:0+i])
-            if (low_min_C > low_temp):
-                low_min_C = low_temp           
-
-        # Buy Strategy - HUNT
-        if (state_A == 0):
-            if (current_price_A < low_mean_A and current_price_A > high_min_A):
-                krw = (get_balance("KRW"))
-                if krw > 5000:
-                    upbit.buy_market_order(ticker_A, krw*0.9995)
-                    state_A = 1
-                buy_price_A = current_price_A
-                kkk_A = 0
-
-        # Buy Strategy - STRK
-        if (state_B == 0):
-            if (current_price_B < low_mean_B and current_price_B > high_min_B):
-                krw = (get_balance("KRW"))
-                if krw > 5000:
-                    upbit.buy_market_order(ticker_B, krw*0.9995)
-                    state_B = 1
-                buy_price_B = current_price_B
-                kkk_B = 0
-
-        # Buy Strategy - STRK
-        if (state_C == 0):
-            if (current_price_C < low_mean_C and current_price_C > high_min_C):
-                krw = (get_balance("KRW"))
-                if krw > 5000:
-                    upbit.buy_market_order(ticker_C, krw*0.9995)
-                    state_C = 1
-                buy_price_C = current_price_C
-                kkk_C = 0
-
-        # Sell Strategy - HUNT
-        if (state_A == 1):
-            if (kkk_A <= 360): ## 1시간 경과
-                percent = 0.015 - 0.015*(kkk_A/360)
-                target_price = buy_price_A*(1+percent)
-                percent = 0.01 - 0.01*(kkk_A/360)
-                lower_price = buy_price_A*(1-percent)
-                if (current_price_A > target_price): 
-                    btc = get_balance(tic_A)
-                    if btc > 0:
-                        upbit.sell_market_order(ticker_A, btc*0.9995)
-                    state_A = 0
-                    kkk_A = 0
-                elif (current_price_A < lower_price):
-                    buy_price_A = current_price_A
-                    state_A = 1
-            else:
-                buy_price_A = current_price_A
-                state_A = 1
-                kkk_A = 360
-            
-            if (current_price_A < low_min_A):
-                btc = get_balance(tic_A)
-                if btc > 0:
-                    upbit.sell_market_order(ticker_A, btc*0.9995)
-                state_A = 0
-                kkk_A = 0
-
-        # Sell Strategy - STRK
-        if (state_B == 1):
-            if (kkk_B <= 360): ## 1시간 경과
-                percent = 0.015 - 0.015*(kkk_B/360)
-                target_price = buy_price_B*(1+percent)
-                percent = 0.01 - 0.01*(kkk_B/360)
-                lower_price = buy_price_B*(1-percent)
-                if (current_price_B > target_price): 
-                    btc = get_balance(tic_B)
-                    if btc > 0:
-                        upbit.sell_market_order(ticker_B, btc*0.9995)
-                    state_B = 0
-                    kkk_B = 0
-                elif (current_price_B < lower_price):
-                    buy_price_B = current_price_B
-                    state_B = 1
-            else:
-                buy_price_B = current_price_B
-                state_B = 1
-                kkk_B = 360
-            
-            if (current_price_B < low_min_B):
-                btc = get_balance(tic_B)
-                if btc > 0:
-                    upbit.sell_market_order(ticker_B, btc*0.9995)
-                state_B = 0
-                kkk_B = 0
-
-        # Sell Strategy - STRK
-        if (state_C == 1):
-            if (kkk_C <= 360): ## 1시간 경과
-                percent = 0.015 - 0.015*(kkk_C/360)
-                target_price = buy_price_C*(1+percent)
-                percent = 0.01 - 0.01*(kkk_C/360)
-                lower_price = buy_price_C*(1-percent)
-                if (current_price_C > target_price): 
-                    btc = get_balance(tic_C)
-                    if btc > 0:
-                        upbit.sell_market_order(ticker_C, btc*0.9995)
-                    state_C = 0
-                    kkk_C = 0
-                elif (current_price_C < lower_price):
-                    buy_price_C = current_price_C
-                    state_C = 1
-            else:
-                buy_price_C = current_price_C
-                state_C = 1
-                kkk_C = 360
-            
-            if (current_price_C < low_min_C):
-                btc = get_balance(tic_C)
-                if btc > 0:
-                    upbit.sell_market_order(ticker_C, btc*0.9995)
-                state_C = 0
-                kkk_C = 0
+        if (now.minute != minute_pre):
+            kkk_1 = kkk_1 + 1
+            kkk_2 = kkk_2 + 1
+            kkk_3 = kkk_3 + 1
+            kkk_4 = kkk_4 + 1
+            kkk_5 = kkk_5 + 1
+            kkk_6 = kkk_6 + 1
+            kkk_7 = kkk_7 + 1
+            kkk_8 = kkk_8 + 1
+            kkk_9 = kkk_9 + 1
+            kkk_10 = kkk_10 + 1
+           
+        kkk_1, state_1, buy_price_1 = strategy(ticker_1, tic_1, kkk_1, state_1, buy_price_1, ratio_1)
+        kkk_2, state_2, buy_price_2 = strategy(ticker_2, tic_2, kkk_2, state_2, buy_price_2, ratio_2)
+        kkk_3, state_3, buy_price_3 = strategy(ticker_3, tic_3, kkk_3, state_3, buy_price_3, ratio_3)
+        kkk_4, state_4, buy_price_4 = strategy(ticker_4, tic_4, kkk_4, state_4, buy_price_4, ratio_4)
+        kkk_5, state_5, buy_price_5 = strategy(ticker_5, tic_5, kkk_5, state_5, buy_price_5, ratio_5)
+        kkk_6, state_6, buy_price_6 = strategy(ticker_6, tic_6, kkk_6, state_6, buy_price_6, ratio_6)
+        kkk_7, state_7, buy_price_7 = strategy(ticker_7, tic_7, kkk_7, state_7, buy_price_7, ratio_7)
+        kkk_8, state_8, buy_price_8 = strategy(ticker_8, tic_8, kkk_8, state_8, buy_price_8, ratio_8)
+        kkk_9, state_9, buy_price_9 = strategy(ticker_9, tic_9, kkk_9, state_9, buy_price_9, ratio_9)
+        kkk_10, state_10, buy_price_10 = strategy(ticker_10, tic_10, kkk_10, state_10, buy_price_10, ratio_10)
 
         minute_pre = now.minute
         # Print
         print(now.hour,'/',now.minute,'/',now.second)
-        print('BTC-','st:',state_A,'/k:',kkk_A,'/bp:',buy_price_A,'/current:',current_price_A,'/mean:',round(low_mean_A,1),'/low:',low_min_A,'/high:',high_min_A)
-        print('ETH-','st:',state_B,'/k:',kkk_B,'/bp:',buy_price_B,'/current:',current_price_B,'/mean:',round(low_mean_B,1),'/low:',low_min_B,'/high:',high_min_B)
-        print('STRK-','st:',state_C,'/k:',kkk_C,'/bp:',buy_price_C,'/current:',current_price_C,'/mean:',round(low_mean_C,1),'/low:',low_min_C,'/high:',high_min_C)
+        print(tic_1,'-','st:',state_1,'/k:',kkk_1,'/bp:',buy_price_1)
+        print(tic_2,'-','st:',state_2,'/k:',kkk_2,'/bp:',buy_price_2)
+        print(tic_3,'-','st:',state_3,'/k:',kkk_3,'/bp:',buy_price_3)
+        print(tic_4,'-','st:',state_4,'/k:',kkk_4,'/bp:',buy_price_4)
+        print(tic_5,'-','st:',state_5,'/k:',kkk_5,'/bp:',buy_price_5)
+        print(tic_6,'-','st:',state_6,'/k:',kkk_6,'/bp:',buy_price_6)
+        print(tic_7,'-','st:',state_7,'/k:',kkk_7,'/bp:',buy_price_7)
+        print(tic_8,'-','st:',state_8,'/k:',kkk_8,'/bp:',buy_price_8)
+        print(tic_9,'-','st:',state_9,'/k:',kkk_9,'/bp:',buy_price_9)
+        print(tic_10,'-','st:',state_10,'/k:',kkk_10,'/bp:',buy_price_10)
+        
+        #print('BTC-','st:',state_A,'/k:',kkk_A,'/bp:',buy_price_A,'/current:',current_price_A,'/mean:',round(low_mean_A,1),'/low:',low_min_A,'/high:',high_min_A)
+        #print('ETH-','st:',state_B,'/k:',kkk_B,'/bp:',buy_price_B,'/current:',current_price_B,'/mean:',round(low_mean_B,1),'/low:',low_min_B,'/high:',high_min_B)
+        #print('STRK-','st:',state_C,'/k:',kkk_C,'/bp:',buy_price_C,'/current:',current_price_C,'/mean:',round(low_mean_C,1),'/low:',low_min_C,'/high:',high_min_C)
 
         time.sleep(1)
     except Exception as e:
